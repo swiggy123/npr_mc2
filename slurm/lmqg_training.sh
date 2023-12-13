bf31d374c1fd30e425ff9e1f97f115c561e32e0c
Gabriel Torres Gamez <gabriel.torres@fuet.ch>
2023-12-13 09:50:34 +0100

    cleanup repo

--- slurm/lmqg_training.sh
+++ slurm/lmqg_training.sh
@@ -4,11 +4,11 @@
 #SBATCH --mem=16G
 #SBATCH --gres=gpu:1
 #SBATCH --partition=performance
-#SBATCH --out=slurm/log_out.txt
-#SBATCH --err=slurm/log_err.txt
+#SBATCH --out=slurm/training_out.log
+#SBATCH --err=slurm/training_err.log
 #SBATCH --job-name="LMQG Train"
 python3 -m pip install -r requirements.txt
 python3 -m spacy download en_core_web_sm
-python3 -m pip install -e ./lm-question-generation
 
-python3 -m papermill testing_lm_question_generation.ipynb testing_lm_question_generation.ipynb -k 'python3'
+NB_PATH="training.ipynb"
+python3 -m papermill $NB_PATH $NB_PATH -k 'python3'
