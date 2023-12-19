58184a46042b11edc844ef483c0903d6ae7c373b
Gabriel Torres Gamez <gabriel.torres@fuet.ch>
2023-12-19 17:06:51 +0100

    train and eval base model

--- slurm/lmqg_push_to_hf.sh
+++ slurm/lmqg_push_to_hf.sh
@@ -7,9 +7,10 @@
 #SBATCH --out=slurm/logs/push_to_hf_out.txt
 #SBATCH --err=slurm/logs/push_to_hf_err.txt
 #SBATCH --job-name="LMQG push to HF"
+#SBATCH --exclude=gpu23a,gpu23b,gpu23c,gpu23d
 python3 -m pip install -r requirements.txt
 python3 -m spacy download en_core_web_sm
 python3 -m pip install -e ./lm-question-generation
 
-lmqg-push-to-hf -m "small_finetuned_ckpt/best_model" -a "t5-small-newsqa-qag-finetuned" -o "StellarMilk" --use-auth-token
-lmqg-push-to-hf -m "small_trained_ckpt/best_model" -a "t5-small-newsqa-qag-trained" -o "StellarMilk" --use-auth-token
+#lmqg-push-to-hf -m "small_finetuned_ckpt/best_model" -a "t5-small-newsqa-qag-finetuned" -o "StellarMilk" --use-auth-token
+lmqg-push-to-hf -m "base_trained_ckpt/best_model" -a "t5-base-newsqa-qag-trained" -o "StellarMilk" --use-auth-token
