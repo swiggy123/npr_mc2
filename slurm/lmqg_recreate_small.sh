64759df8a6ea98e2797af6cc049592aaca41e443
Gabriel Torres Gamez <gabriel.torres@fuet.ch>
2024-01-07 13:26:06 +0100

    add recreated model

---
+++ slurm/lmqg_recreate_small.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#SBATCH --time=7-00:00:00
+#SBATCH --cpus-per-task=2
+#SBATCH --mem=16G
+#SBATCH --gres=gpu:1
+#SBATCH --partition=performance
+#SBATCH --out=slurm/logs/recreated_small_out.txt
+#SBATCH --err=slurm/logs/recreated_small_err.txt
+#SBATCH --job-name="LMQG Small Recreated"
+#SBATCH --exclude=gpu22a,gpu22b,node15,sdas2
+python3 -m pip install -r requirements.txt
+python3 -m spacy download en_core_web_sm
+python3 -m pip install -e ./lm-question-generation
+
+lmqg-train-search -d "lmqg/qag_squad" -m "t5-small" -b 2 -g 2 4 -c "small_recreated_ckpt" -i 'paragraph' -o 'questions_answers' -p 'qag' --epoch-partial 10 -e 15 --max-length-output-eval 256 --max-length-output 256
